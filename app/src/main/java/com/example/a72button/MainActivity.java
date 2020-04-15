6aa332e008354c11345bb420bacb7186d4f55a90
Pyry-Santahuhta <61690808+pyry-santahuhta@users.noreply.github.com>
2020-04-15 14:46:41 +0300

    Second update

--- app/src/main/java/com/example/a72button/MainActivity.java
+++ app/src/main/java/com/example/a72button/MainActivity.java
@@ -17,8 +17,6 @@ import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
-import android.widget.CheckBox;
-import android.widget.ListView;
 import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.TimePicker;
